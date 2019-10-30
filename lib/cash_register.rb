6d4ad9c9f27bb18a42838e9de457f916e5d98a09
imade osifo <imadejr@yahoo.com>
2019-10-30 20:03:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
-
+class CashRegister
+    
+    ITEMS = [] 
+    attr_accessor :total, :discount 
+    
+   def initialize(given_discount = 100) 
+       @total = 0 
+       @discount = given_discount 
+   end 
+    
+   def add_item(item, price, multiplier = 1)
+      multiplier.times do ITEMS << item
+      
+      @total += (price * multiplier)
+   end 
+   
+   def apply_discount()
+     @total = (@total * ((100.0 - @discount ) /100)).to_i 
+     
+     if @total == 0 
+        p "There is no discount to apply."
+     else 
+        p"After the discount, the total comes to $#{@total}." 
+     end 
+    # @total = @total * (@discount / 100.0) 
+    end 
+    
+    def items 
+       ITEMS
+    end 
+    
+    
+    
+end 
